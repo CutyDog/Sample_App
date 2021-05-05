5df2ac18c9f42f9bfc08f429aff610398669bf7f
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-1.compute.internal>
2021-05-05 11:45:30 +0000

    create user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
   end 
   
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      
+    else
+      render 'new'
+    end  
+  end  
   
 end
