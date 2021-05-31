9c1850fdcf392d65c68340895904260a466dfd44
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-1.compute.internal>
2021-05-31 07:19:46 +0000

    add error message to SinUp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,18 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       
     else
       render 'new'
     end  
   end  
+  
+  private  
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end  
   
 end
