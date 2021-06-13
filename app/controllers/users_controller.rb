48958210232bebf17938732d828db679222e108c
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-1.compute.internal>
2021-06-13 09:57:27 +0000

    authorization model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -21,11 +23,9 @@ class UsersController < ApplicationController
   end  
   
   def edit
-    @user = User.find(params[:id])
   end  
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -40,4 +40,19 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end  
   
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end  
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end  
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end  
   
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: {user: {name: "",
@@ -16,9 +17,27 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end  
   
-  test "successful edit" do
+  # test "successful edit" do
+  #   log_in_as(@user)
+  #   get edit_user_path(@user)
+  #   assert_template 'users/edit'
+  #   name = "Foo Bar"
+  #   email = "foo@bar.com"
+  #   patch user_path(@user), params: {user: {name: name,
+  #                                           email: email,
+  #                                           password: "",
+  #                                           password_confirmation: ""}}
+  #   assert_not flash.empty?
+  #   assert_redirected_to @user
+  #   @user.reload
+  #   assert_equal name, @user.name
+  #   assert_equal email, @user.email
+  # end 
+  
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: {user: {name: name,
@@ -30,5 +49,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+    
+    delete logout_path
+    log_in_as(@user)
+    assert_redirected_to @user
   end  
 end
