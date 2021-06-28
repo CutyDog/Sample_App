774a8dccc60997c6f65b855c61db7544878e9636
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-1.compute.internal>
2021-06-28 15:05:41 +0000

    setup s3 bucket

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'],
-      :aws_access_key_id     => ENV['AKIAWL3HJNRWINDSDZH6'],
-      :aws_secret_access_key => ENV['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']
+      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['taishibucket']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
