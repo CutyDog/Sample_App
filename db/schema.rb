ead8a4ea19db9afc907d36b2e41291fc7896fb51
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-1.compute.internal>
2021-07-12 18:07:52 +0000

    change Gem(2)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210702002712) do
+ActiveRecord::Schema.define(version: 2021_07_02_002712) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
