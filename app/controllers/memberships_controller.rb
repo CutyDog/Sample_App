95bf0bcb2ad9711fb60af79c30fce4db7dfe9f8c
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-1.compute.internal>
2021-07-27 10:56:36 +0000

    memberships_controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,7 +1,29 @@
 class MembershipsController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_user, only: :destroy
+  
+  
   def create
+    @talk = Talk.find(:talk_id)
+    params[:guest_users].each do |guest_user|
+      if @talk.member?(guest_user)
+        
+      else
+        @talk.add_member(guest_user)
+      end
+    end
   end
   
   def destroy
+    @membership.destroy
+    flash[:success] = "Left the Talk"
+    redirect_back(fallback_location: root_url)
   end
+  
+  private
+  
+    def correct_user
+      @membership = current_user.memberships.find_by(id: params[:id])
+      redirect_to root_url if @membership.nil?
+    end
 end
