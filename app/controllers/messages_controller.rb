cd418995acf5581d5522e7b49bb7b2c2f2c2f79a
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-1.compute.internal>
2021-07-27 11:01:46 +0000

    messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,18 @@
 class MessagesController < ApplicationController
-  def create
+  before_action :logged_in_user
+  before_action :correct_user, only: :destroy
+  
+  
+  def destroy
+    @talk = @message.talk
+    @message.destroy
+    flash[:success] = "Delete the Message"
+    redirect_to @talk
   end
   
-  
+  private
+    def correct_user
+      @message = current_user.messages.find_by(id: params[:id])
+      redirect_to root_url if @messages.nil?
+    end
 end
