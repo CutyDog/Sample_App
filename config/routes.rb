86a14a6247bdc207de2fb25507aa4d0120d37213
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-1.compute.internal>
2021-08-06 01:36:14 +0000

    REST API for static_pages,talks_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   get 'messages/create'
   root 'static_pages#home'  
+  get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
